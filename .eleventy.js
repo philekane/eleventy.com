72036ec47a320a7c88bf12d5952f319d8fb9fefc
Phil Kane <pkane@spindry.com>
2020-04-03 10:40:43 -0700

    moved admin folder to src

--- .eleventy.js
+++ .eleventy.js
@@ -6,7 +6,7 @@ module.exports = function(eleventyConfig){
     return{
         passthroughFileCopy: true,
         markdownTemplateEngine: "njk",
-        templateFormats: ["html", "njk", "md" ],
+        templateFormats: ["html", "njk", "md", "yml" ],
         dir:{
             input: "src",
             output: "_site",
