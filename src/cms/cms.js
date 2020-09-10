2c3fb53d7b77645be8e3fb125aa90be7446e309f
Phil Kane <pkane@spindry.com>
2020-09-10 10:47:51 -0700

    capitalized Color

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,4 +1,4 @@
 import CMS from 'netlify-cms-app';
 import ColorControl from "netlify-cms-widget-colorpicker";
-CMS.registerWidget("color", ColorControl);
+CMS.registerWidget("Color", ColorControl);
 CMS.init();
