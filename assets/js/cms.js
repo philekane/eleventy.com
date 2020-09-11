a95cde08bb42f71ad993d7732f95bbad44f2398a
Phil Kane <pkane@spindry.com>
2020-09-11 10:05:14 -0700

    added ../node-modules/

--- assets/js/cms.js
+++ assets/js/cms.js
@@ -1,5 +1,4 @@
 //import CMS from 'netlify-cms-app';
-import ColorControl from "../netlify-cms-widget-colorpicker";
+import ColorControl from "../node-modules/netlify-cms-widget-colorpicker";
 CMS.registerWidget("color", ColorControl);
 //CMS.init();
-t
