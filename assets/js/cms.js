009f788b85809b386fdbbd44eb23b15787680d5d
Phil Kane <pkane@spindry.com>
2020-09-11 12:18:24 -0700

    added some build in the script

--- assets/js/cms.js
+++ assets/js/cms.js
@@ -1,4 +1,4 @@
-import CMS from '/node_modules/netlify-cms-app';
+//import CMS from '/node_modules/netlify-cms-app';
 import ColorControl from "/node_modules/netlify-cms-widget-colorpicker";
 CMS.registerWidget("color", ColorControl);
 //CMS.init();
