49e327a299c367dc2d5e57d4f34c197ecb9ffa4e
Phil Kane <pkane@spindry.com>
2020-09-14 13:46:36 -0700

    removed cms/cms.js and went back to cdn on the index.html

--- assets/js/cms.js
+++
@@ -1,4 +0,0 @@
-import CMS from "/node_modules/netlify-cms";
-import * as ColorWidget from "/node_modules/netlify-cms-widget-color";
-CMS.registerWidget("color", ColorWidget.Control);
-//CMS.init();
